0d9a6a1fd7cf19ed8ec4888eb8708b56f4ebf3b0
Alex Scotland <alexscotland@Alexs-MacBook-Pro.local>
2023-08-26 22:21:09 -0400

    adds file changes to have API wrapper seperate class

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const axios = require('axios');
-
+// Internal Imports
+const OpenAIApiWrapper = require('./lib/api-wrappers/open-ai-wrapper');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -11,7 +9,7 @@ const axios = require('axios');
  */
 async function activate(context) {
 	// Let's let the user enter their API Key
-	const open_api_key = await vscode.window.showInputBox({
+	let open_api_key = await vscode.window.showInputBox({
 		placeHolder: 'XXXXXXXXXXXXXXXX',
 		prompt: 'OpenAI API Key',
 		title: 'OpenAI API Key',
@@ -19,13 +17,15 @@ async function activate(context) {
 		password: true
 	});
 
-	const open_api_organization = await vscode.window.showInputBox({
+	let open_api_organization = await vscode.window.showInputBox({
 		placeHolder: 'XXXXXXXXXXXXXXXX',
 		prompt: 'OpenAI Organization Key',
 		title: 'OpenAI Organization Key',
 		ignoreFocusOut: true,
 		password: true
 	});
+
+	const open_api = new OpenAIApiWrapper(open_api_key, open_api_organization)
 	
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -36,39 +36,10 @@ async function activate(context) {
 			title: 'Ask a Question',
 			ignoreFocusOut: true,
 		});
-		console.log(question_to_ask)
 		// The code you place here will be executed every time your command is executed
-		axios.post(
-			'https://api.openai.com/v1/chat/completions',
-			{
-				"model": "gpt-3.5-turbo",
-				"messages": [
-					{
-					"role": "system",
-					"content": "This conversation involves a developer asking for help."
-					},
-					{
-					"role": "user",
-					"content": question_to_ask
-					}
-				]
-			},
-			{
-				headers: {
-					'Content-Type': 'application/json',
-					"Authorization": `Bearer ${open_api_key}`,
-					"OpenAI-Organization": open_api_organization
-				}
-			}
-		)
-		.then(function (response) {
-			let message = response['data']['choices'][0]['message']['content'];
-			vscode.window.showInformationMessage(message)
-		  })
-		  .catch(function (error) {
-			console.log(error);
-		  });
+		answer = await open_api.post_data_question(question_to_ask)
 		// Display a message box to the user
+		vscode.window.showInformationMessage(answer)
 		
 	});
 
